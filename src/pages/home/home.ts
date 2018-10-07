45cb3e80753ca8497c53aa648351212c1891062d
hristo2612 <hristoapps@gmail.com>
2018-10-07 04:14:32 +0300

    Include Home Component

---
+++ src/pages/home/home.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  data = { title:'', description:'', date:'', time:'' };
+
+  constructor(public navCtrl: NavController,
+    public localNotifications: LocalNotifications,
+    public platform: Platform,
+    public alertCtrl: AlertController) {}
+
+  submit() {
+    console.log(this.data);
+    //var date = new Date(this.data.date+" "+this.data.time);
+    const time: Date = new Date(); // tslint:disable-line
+    time.setSeconds(time.getSeconds() + 69);
+    //console.log(date);
+    this.localNotifications.requestPermission().then((permission) => {
+      this.localNotifications.schedule({
+        id: 1,
+        title: 'Just do it',
+        at: time,
+        text: 'Beat The Habbit, stay strong.. bitch',
+        sound: this.setSound()
+      });
+      // this.localNotifications.schedule({
+      //    id: 1,
+      //    text: 'Delayed ILocalNotification',
+      //    at: date,
+      //    led: 'FF0000',
+      //    sound: this.setSound(),
+      // });
+      // let alert = this.alertCtrl.create({
+      //   title: 'Congratulation!',
+      //   subTitle: 'Notification setup successfully at '+date,
+      //   buttons: ['OK']
+      // });
+      // alert.present();
+      this.data = { title:'', description:'', date:'', time:'' };
+    });
+  }
+
+  setSound() {
+    if (this.platform.is('android')) {
+      return 'file://assets/sounds/Rooster.mp3'
+    } else {
+      return 'file://assets/sounds/Rooster.caf'
+    }
+  }
+
+}
