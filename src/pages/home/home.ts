c0a7616b6c1ca4f7aab84428c168f1ff3ddce4e1
hristo2612 <hristoapps@gmail.com>
2018-10-08 18:50:26 +0300

    Check if we are running current app on real device or in Browser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,31 +19,34 @@ export class HomePage {
     console.log(this.data);
     //var date = new Date(this.data.date+" "+this.data.time);
     const time: Date = new Date(); // tslint:disable-line
-    time.setSeconds(time.getSeconds() + 69);
+    time.setSeconds(time.getSeconds() + 6);
     //console.log(date);
-    this.localNotifications.requestPermission().then((permission) => {
-      this.localNotifications.schedule({
-        id: 1,
-        title: 'Just do it',
-        at: time,
-        text: 'Beat The Habbit, stay strong.. bitch',
-        sound: this.setSound()
+    if ( this.platform.is('cordova') ) {
+      this.localNotifications.requestPermission().then((permission) => {
+        this.localNotifications.schedule({
+          id: 1,
+          title: 'Just do it',
+          at: time,
+          text: 'Beat The Habbit, stay strong.. bitch',
+          sound: this.setSound()
+        });
+        // this.localNotifications.schedule({
+        //    id: 1,
+        //    text: 'Delayed ILocalNotification',
+        //    at: date,
+        //    led: 'FF0000',
+        //    sound: this.setSound(),
+        // });
+        // let alert = this.alertCtrl.create({
+        //   title: 'Congratulation!',
+        //   subTitle: 'Notification setup successfully at '+date,
+        //   buttons: ['OK']
+        // });
+        // alert.present();
+        this.data = { title:'', description:'', date:'', time:'' };
       });
-      // this.localNotifications.schedule({
-      //    id: 1,
-      //    text: 'Delayed ILocalNotification',
-      //    at: date,
-      //    led: 'FF0000',
-      //    sound: this.setSound(),
-      // });
-      // let alert = this.alertCtrl.create({
-      //   title: 'Congratulation!',
-      //   subTitle: 'Notification setup successfully at '+date,
-      //   buttons: ['OK']
-      // });
-      // alert.present();
-      this.data = { title:'', description:'', date:'', time:'' };
-    });
+    }
+
   }
 
   setSound() {
