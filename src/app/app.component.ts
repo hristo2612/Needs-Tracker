e2c4dbb84dd87a794efd02a6e6f9900a53c4b534
hristo2612 <hristoapps@gmail.com>
2018-11-03 23:33:42 +0200

    Update app.component - add sample storage saving - add openPage method for opening our 2 pages - history and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
+import { HistoryPage } from '../pages/history/history';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+    this.storage.set('name', 'Max');
+    this.storage.set('age', '69').then(() => {
+      this.storage.get('age').then((val) => {
+        console.log('Your age is', val);
+      });
+    });
+  }
+
+  openPage(page): void {
+    if (page === 'home') {
+      this.nav.goToRoot({});
+    } else if (page === 'history') {
+      if (!(this.nav.getActive().name === "HistoryPage")) {
+        this.nav.push(HistoryPage);
+      }
+    }
   }
 }
 
