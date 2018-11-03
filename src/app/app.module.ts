4fe743a74ee0de9ba7e8e94d29aae2436609533a
hristo2612 <hristoapps@gmail.com>
2018-11-03 23:30:43 +0200

    Update app module - include all pages and components needed - also add storage module for storing your personal progress locally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HistoryPage } from '../pages/history/history';
+import { ProgressBarComponent } from '../components/progress-bar/progress-bar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    HistoryPage,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    HistoryPage
   ],
   providers: [
     StatusBar,
