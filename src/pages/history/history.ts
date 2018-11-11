139891fd80a962c335c15751abf5ab01ba74dddb
hristo2612 <hristoapps@gmail.com>
2018-11-11 23:17:34 +0200

    Refactor history component - now you can view the overall history ( for every logged day )

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
 import { Chart } from "chart.js";
 import { StoreProvider } from "../../providers/store/store";
+import Avg from 'average-array';
 
 Chart.defaults.global.legend.display = false;
 @Component({
@@ -23,10 +24,17 @@ export class HistoryPage {
   selectedBarChartClass: string = 'selected';
   selectedLineChartClass: string = '';
 
-  selectedTimeFrame: string = 'Today';
+  selectedTimeFrame: string = 'This Week';
+
+  allLabels: string[] = [];
+  selectedLabel: string = '';
+
+  yearLength: number = 365;
+  monthLength: number = 31;
+  weekLength: number = 7;
 
   progressForToday: any = [];
-  progressForPreviousDays: any = [];
+  allProgressBars: any = [];
   progressBarLabels: any = [];
   progressBarValues: any = [];
 
@@ -57,19 +65,89 @@ export class HistoryPage {
     console.log(period);
   }
 
+  onLabelChange(label) {
+    this.selectedLabel = label;
+    this.renderLineChart();    
+  }
+
   ionViewDidLoad() {
     this.store.getAllProgress().then((allProgress) => {
       if (allProgress) {
-        console.log(allProgress);
-        this.progressForPreviousDays = allProgress;
+        this.allProgressBars = allProgress;
       }
       this.store.getProgress().then((progress) => {
+        let currentDate = new Date();
+        // push today into all our bars array
+        this.allProgressBars.push({ date: { 
+          date: {
+            day: currentDate.getDate(), 
+            month: currentDate.getMonth()
+          }
+        }, bars: progress });
+        this.allProgressBars = this.allProgressBars.slice().reverse();
         this.progressForToday = progress;
+        this.allLabels = this.renderLabels(this.progressForToday);
+        this.selectedLabel = this.allLabels[0] || '';
         this.barChart = this.renderBarChart();
-      });
+        this.lineChart = this.renderLineChart();
+  });
     });
+  }
 
-    this.lineChart = this.renderLineChart();
+  averageValuesFor(all, type) {
+    return Avg(
+      this.store.concatAll(
+        all.map((progress) => {
+          return progress.bars.filter((item) => {
+            return item.need === type;
+          }).map((bar) => {
+            return bar.percent;
+          })
+        })
+      )
+    );
+  }
+
+  renderLabels(bars) {
+    return bars.map((target) => {
+      return target.need;
+    });
+  }
+
+  renderValues(bars) {
+    return bars.map((target) => {
+      return target.percent;
+    });
+  }
+
+  renderColors(bars, opacity) {
+    return bars.map((target) => {
+      let color = null;
+      if (target.percent > 75) {
+        color = `rgb(30, 193, 7, ${opacity})`;
+      } else if (target.percent > 59) {
+        color = `rgb(213,219,33, ${opacity})`;
+      } else if (target.percent > 29) {
+        color = `rgb(219, 108, 34, ${opacity})`;
+      } else {
+        color = `rgb(199, 35, 17, ${opacity})`;
+      }
+      return color;
+    });
+  }
+
+  checkSelectedTime(selected) {
+    if (selected === 'Today') {
+
+    } else if (selected === 'Yesterday') {
+
+    } else if (selected === 'This Week') {
+
+    } else if (selected === 'This Month') {
+
+    } else if (selected === 'This Year') {
+
+    }
   }
 
   renderBarChart() {
@@ -101,47 +179,31 @@ export class HistoryPage {
     });
   }
 
-  renderLabels(bars) {
-    return bars.map((target) => {
-      return target.need;
-    });
-  }
-
-  renderValues(bars) {
-    return bars.map((target) => {
-      return target.percent;
-    });
-  }
-
-  renderColors(bars, opacity) {
-    return bars.map((target) => {
-      let color = null;
-      if (target.percent > 75) {
-        color = `rgb(30, 193, 7, ${opacity})`;
-      } else if (target.percent > 59) {
-        color = `rgb(213,219,33, ${opacity})`;
-      } else if (target.percent > 29) {
-        color = `rgb(219, 108, 34, ${opacity})`;
-      } else {
-        color = `rgb(199, 35, 17, ${opacity})`;
+  mapLineChartData(all, from, to, label) {
+    let chartData = {
+      labels:[],
+      data:[]
+    };
+    all.forEach((item, index) => {
+      if (index >= from && index <= to) {
+        chartData.labels.push(item.date.date.day + '/' + item.date.date.month);
+        // get percentages for selected label a.k.a need
+        item.bars.forEach((bar) => {
+          if (label === bar.need) {
+            chartData.data.push(bar.percent);
+          }
+        })
       }
-      return color;
     });
+    return chartData;
   }
 
   renderLineChart() {
+    let chartData = this.mapLineChartData(this.allProgressBars, 0,6, this.selectedLabel);
     return new Chart(this.lineCanvas.nativeElement, {
       type: "line",
       data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
+        labels: chartData.labels,
         datasets: [
           {
             fill: false,
@@ -161,7 +223,7 @@ export class HistoryPage {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: chartData.data,
             spanGaps: false
           }
         ]
