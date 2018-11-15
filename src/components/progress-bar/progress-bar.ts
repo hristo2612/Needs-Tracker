6dd884a4805505c8cb2d66d7456f9b910d7a88e8
hristo2612 <hristoapps@gmail.com>
2018-11-15 20:18:48 +0200

    Add swiping functionality for progressbar component
    also add percent toggle classes for styling the padding

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { StoreProvider } from '../../providers/store/store';
+import { IProgress } from '../../app/models/progress';
 
 @Component({
   selector: 'progress-bar',
@@ -8,6 +9,7 @@ import { StoreProvider } from '../../providers/store/store';
 export class ProgressBarComponent {
   private _progress: string;
   className: string;
+  @Input() index: number;
 
   get progress(): string {
     return this._progress;
@@ -15,6 +17,13 @@ export class ProgressBarComponent {
 
   @Input()
   set progress(progress: string) {
+    this.setProgress(progress);
+  }
+
+  constructor(private store: StoreProvider) {
+  }
+
+  setProgress(progress: string) {
     this._progress = progress;
     let numberProgress = +progress;
     if (numberProgress > 75) {
@@ -28,7 +37,37 @@ export class ProgressBarComponent {
     }
   }
 
-  constructor(private store: StoreProvider) {
+  swipe(event) {
+    if (event.direction === 4 && this._progress !== '100') {
+      // swiped forward
+      this.setProgress((parseInt(this._progress) + 1) + '');
+    } else if (event.direction === 2 && this._progress !== '1') {
+      // swiped backward
+      this.setProgress((parseInt(this._progress) - 1) + '');
+    }
+    // swipe (pan) is final
+    if (event.isFinal) {
+      this.saveProgress();
+    }
+    console.log(event);
   }
 
+  saveProgress() {
+    this.getProgressData().then((progressData) => {
+      let currentProgress = progressData;
+      currentProgress[this.index].percent = parseInt(this._progress);
+      this.setProgressData(currentProgress).then((progress) => {
+        //this.dismiss();
+        console.log('progress data saved')
+      });
+    });
+  }
+
+  getProgressData(): Promise<IProgress[]> {
+    return this.store.getProgress();
+  }
+
+  setProgressData(data: IProgress[]): Promise<IProgress[]> {
+    return this.store.setProgress(data);
+  }
 }
